39f5ee675876b5c6c725a1758ce75e90f61c4943
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2024-04-19 00:08:26 +0200

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+// src/App.jsx
+
+// src/App.jsx
+
+import Product from "./Product";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
---
+++ src/components/Product.jsx
@@ -0,0 +1,12 @@
+// src/Product.jsx
+
+const Product = () => {
+  return (
+    <div>
+      <h2>Tacos</h2>
+      <p>Price: 999 credits</p>{" "}
+    </div>
+  );
+};
+
+export default Product;
